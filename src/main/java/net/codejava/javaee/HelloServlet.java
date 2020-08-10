49e40f09f56da58ffdd13dfcdfd6d2d906e3f6a1
anilkokitkar82 <60177242+anilkokitkar82@users.noreply.github.com>
2020-08-10 14:54:03 +0530

    Initial check-in

---
+++ src/main/java/net/codejava/javaee/HelloServlet.java
@@ -0,0 +1,39 @@
+package net.codejava.javaee;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/helloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		String firstName = request.getParameter("fn");
+	    String lastName = request.getParameter("ln");
+	 
+	    response.getWriter().append("Full Name: " + firstName + " " + lastName);
+		
+	    response.getWriter().close();
+	}
+
+}
---
+++ src/test/java/net/codejava/javaee/HelloServletTest.java
@@ -0,0 +1,50 @@
+package net.codejava.javaee;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import net.codejava.javaee.HelloServlet;
+ 
+public class HelloServletTest {
+ 
+    @Mock
+    HttpServletRequest request;
+ 
+    @Mock
+    HttpServletResponse response;
+ 
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+ 
+    @Test
+    public void testFullName() throws IOException, ServletException {
+ 
+        when(request.getParameter("fn")).thenReturn("Poonam");
+        when(request.getParameter("ln")).thenReturn("Maniar");
+ 
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+         
+        when(response.getWriter()).thenReturn(pw);
+ 
+        HelloServlet myServlet =new HelloServlet();
+        myServlet.doPost(request, response);
+        String result = sw.getBuffer().toString().trim();
+        assertEquals(result, new String("Full Name: Poonam Maniar"));
+ 
+    }
+}
